29466becbb0a419cb6ace32869f60fb3e9d79324
hatake04 <michaelangelo002@hotmail.com>
2025-03-30 10:42:29 -0400

    Update Contraseña.java

--- tomdo/class/Contraseña.java
+++ tomdo/class/Contraseña.java
@@ -1,28 +1,45 @@
 package tomdo;
-
-/**
- * Contraseña Class - The Constraseña class is the blueprint of the object 
- */
 import org.mindrot.jbcrypt.BCrypt;
 
+/**
+ * Contraseña Class - The Constraseña class is the blueprint of the object instantiated when
+ * the user enters a password. It is also in charge to encrypt the data for storage in the
+ * system's database using the BCrypt library.  
+ */
 public class Contraseña {
 
 	private String pwd;
 	private String hashPwd;
 	
-	public Contraseña(String ingresoUsuario)
+	/**
+	 * Parameterized Constructor - Creates a password object
+	 * @param pwd
+	 */
+	public Contraseña(String pwd)
 	{
-		pwd = ingresoUsuario;
+		this.pwd = pwd;
 		hashPwd = BCrypt.hashpw(pwd, BCrypt.gensalt(15));
 	}
 	
 	
+	public boolean checkLogin(String data){
+		return BCrypt.checkpw(data, hashPwd);
+	}
+	
+	public String getPwd()	{
+		return pwd;
+	}
+	
+	public String getHashPwd() {
+		return hashPwd;
+	}
 	public void setPwd(String nuevoPwd) {
 		pwd = nuevoPwd;
 	}
 	
-	public boolean checkLogin(String dato){
-		return BCrypt.checkpw(dato, hashPwd);
+	public void setHashPwd(String nuevoHash)
+	{
+		hashPwd = nuevoHash;
 	}
 	
 	public String toString(){
