c806657ea53873c2d60bb7d5e800330ce0d291cf
hatake04 <michaelangelo002@hotmail.com>
2025-05-06 08:03:01 -0400

    Create BusquedaBean.java

---
+++ tomdo/class/BusquedaBean.java
@@ -0,0 +1,16 @@
+package tomdo;
+
+public class BusquedaBean {
+
+	protected String trabajo;
+	protected String lugar;
+	
+	public BusquedaBean(){
+		
+	}
+	
+	public String getTrabajo() {return trabajo;}
+	public String getLugar() {return lugar;}
+	public void setTrabajo(String trabajo) { this.trabajo = trabajo;}
+	public void setLugar(String lugar) {this.lugar = lugar;}
+}
